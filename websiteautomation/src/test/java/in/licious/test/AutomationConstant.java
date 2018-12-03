64974ff45ac99623f9801ea3914bd50221b639d5
System Administrator <root@admins-MacBook-Pro-3.local>
2018-12-03 19:23:43 +0530

    changed the file location as per mac os

--- websiteautomation/src/test/java/in/licious/test/AutomationConstant.java
+++ websiteautomation/src/test/java/in/licious/test/AutomationConstant.java
@@ -4,7 +4,10 @@ import in.licious.util.Helper;
 
 public interface AutomationConstant {
 
-	String EXTENT_REPORT_PATH=System.getProperty("user.dir")+"\\ExtentReports\\"+Helper.dateAndTimeStamp()+"-ereport.html";
+	//String EXTENT_REPORT_PATH=System.getProperty("user.dir")+"\\ExtentReports\\"+Helper.dateAndTimeStamp()+"-ereport.html";
+	
+	String EXTENT_REPORT_PATH=System.getProperty("user.dir")+"/ExtentReports/"+Helper.dateAndTimeStamp()+"-ereport.html";
+	
 	//String SCREEN_SHOT_PATH=System.getProperty("user.dir")+"\\ScreenShots\\"+
 	String Excel_Path=System.getProperty("user.dir")+"\\ExcelData\\"+Helper.dateAndTimeStamp()+"-ereport.html";
 }
